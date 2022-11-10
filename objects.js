9544a4d65fd23027a2427f38182a6c61c8683ba5
sana-patel <sana.patels@gmail.com>
2022-11-10 14:56:46 +0530

    10-11-2022 assignment done on array and object destructuring

--- objects.js
+++ objects.js
@@ -36,3 +36,32 @@ function display(id, name, role) {
 //         console.log("Role", role);
 //     }
 // }
+console.log("*********************************************************");
+console.log("==Assignment on array===========");
+console.log("====Given array=====");
+var array = ["J&J", "pfizer", "SP utnick",];
+console.log(array);
+console.log("====array after adding covaxin");
+array.push("Covaxin");
+console.log(array);
+console.log("===Traversing array====");
+for (var _i = 0, array_1 = array; _i < array_1.length; _i++) {
+    var element = array_1[_i];
+    console.log(element);
+}
+console.log("===Array destructuring=====");
+var v1 = array[0], v2 = array[1], v3 = array[2], v4 = array[3];
+console.log(v1, v2, v3, v4);
+console.log('***********************************************************************');
+console.log("=======Assignment on object=========");
+var s1 = {
+    firstName: "sana",
+    lastName: "Patel",
+    degree: "Msc(physics)",
+    city: "pune"
+};
+console.log(s1);
+console.log("=======object destructuring======");
+var userFirstName = s1.firstName, userLastName = s1.lastName, userDegree = s1.degree, userCity = s1.city;
+console.log(userFirstName, userLastName, userDegree, userCity);
+//console.log(userLastName);
--- objects.ts
+++ objects.ts
@@ -58,3 +58,44 @@ function display(id:number, name:string, role?:string)
 //     }
     
 // }
+console.log("*********************************************************");
+
+console.log("==Assignment on array===========");
+
+ console.log("====Given array=====");
+ 
+var array :any = ["J&J","pfizer","SP utnick",]
+console.log(array);
+
+console.log("====array after adding covaxin");
+array.push("Covaxin")
+console.log(array);
+
+console.log("===Traversing array====");
+
+for (const element of array) {
+    console.log(element);
+    
+}
+
+console.log("===Array destructuring=====");
+var [v1,v2,v3,v4] = array
+console.log(v1,v2,v3,v4);
+
+console.log('***********************************************************************');
+console.log("=======Assignment on object=========");
+
+
+var s1 = {
+    firstName: "sana",
+    lastName: "Patel",
+    degree :"Msc(physics)",
+    city : "pune"
+}
+console.log(s1);
+console.log("=======object destructuring======");
+
+var {firstName: userFirstName, lastName: userLastName, degree:userDegree,city:userCity} = s1;
+
+console.log(userFirstName,userLastName,userDegree,userCity);
+//console.log(userLastName);
